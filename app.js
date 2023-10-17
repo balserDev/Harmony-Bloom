13ebdfb20cdc47c69bc6438233a23dfe0462606d
Mateo Valencia <mateovalencia02@gmail.com>
2023-10-16 18:51:53 -0600

    Added carousel functionality :sparkles:

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import { Calendar, generateCalendars } from './public/scripts/calendar.js';
+import bodyParser from "body-parser";
 
 
 const app = express();
-const port = 3000;
 
+const port = 3000;
+const myCalendars = generateCalendars(2023);
+const currentMonth = (new Date().getMonth()) + 1;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
 
 app.listen(port, (req, res)=>{
@@ -13,11 +19,22 @@ app.listen(port, (req, res)=>{
 
 app.get('/', (req, res)=>{
 
-    const myCalendars = generateCalendars(2023);
-    let currentMonth = (new Date().getMonth()) + 1;
-
     res.render('main.ejs', {
         calendars:myCalendars,
         Month:currentMonth
+    })
+})
+
+app.get('/SUBMIT', (req, res)=>{
+
+    res.send('TEST');
+})
+
+
+app.post('/SUBMIT', (req, res)=>{
+    let month = req.body.currentMonth
+    res.render('main.ejs', {
+        calendars:myCalendars,
+        Month:month
     })
 })
--- public/scripts/menuSelections.js
+++ public/scripts/menuSelections.js
@@ -1,7 +1,36 @@
-$( ".selector" ).click(function(){
-    console.log('Cliked');
-})
+const todaysMonth = new Date().getMonth() + 1;
+
+
+function setCalendars(main){
+    var months = $('.selector');
+    months.css('visibility', 'hidden');
+    
+    $(`.CalendarContainer #${main}`).css('visibility', 'visible');
+    $(`.CalendarContainer #${main}`).removeClass('side_left');
+    $(`.CalendarContainer #${main}`).removeClass('side_right');
+    $('.calendar').removeClass('side');
+    
+    
+    $(`.CalendarContainer #${main + 1}`).css('visibility', 'visible');
+    $(`.CalendarContainer #${main + 1} .calendar`).addClass('side');
+    $(`.CalendarContainer #${main + 1}`).addClass('side_right');
+    
+    $(`.CalendarContainer #${main - 1}`).css('visibility', 'visible');
+    $(`.CalendarContainer #${main - 1} .calendar`).addClass('side');
+    $(`.CalendarContainer #${main - 1}`).addClass('side_left');
+}
+
+setCalendars(todaysMonth);
 
 $('.day').click(function(){
     $(this).css("background-color", "#BB4F4F")
+})
+
+$('.selector').click(function(){
+    console.log('HEY');
+
+    if($(this).hasClass('side_right') || $(this).hasClass('side_left')){
+        id = parseInt($(this).attr('id')) ;
+        setCalendars(id);
+    }
 })
